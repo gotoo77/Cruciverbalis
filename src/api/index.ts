0125b158ee94e2229ec8a55c97e2d244a55674b5
gotoo <gautier.duval@gmail.com>
2026-08-04 07:18:58 +0200

    Export public generation contract

---
+++ src/api/index.ts
@@ -0,0 +1,10 @@
+export {
+  generate,
+  type GeneratedGrid,
+  type GenerationRequest,
+  type GenerationResult,
+  type GenerationStrategy,
+} from './generate';
+
+export type { Entry } from '../core/domain';
+export type { GridQuality } from '../quality/grid-quality';
